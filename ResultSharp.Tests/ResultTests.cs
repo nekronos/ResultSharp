fe7b7e29f73b244ebf48bf6af53898cb9ced0b53
Vegard Strand Lende <lende.vegard@gmail.com>
2020-04-04 00:09:40 +0200

    Add more tests

--- ResultSharp.Tests/ResultTests.cs
+++ ResultSharp.Tests/ResultTests.cs
@@ -112,6 +112,57 @@ namespace ResultSharp.Tests
 		}
 
 		[Fact]
+		public void Map_FaultedResult_ShouldNotMutate()
+		{
+			Result<string, int> result = Err(0);
+
+			var expected = Err(0);
+			var actual = result.Map(str => str.Length);
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void MapOr_OkResult_ShouldReturnMappedValue()
+		{
+			Result<int, string> result = Ok(10);
+
+			var actual = result.MapOr(x => x * 3, -1);
+
+			actual.Should().Be(30);
+		}
+
+		[Fact]
+		public void MapOr_FaultedResult_ShouldReturnDefaultValue()
+		{
+			Result<string, int> result = Err(0);
+
+			var actual = result.MapOr(x => x.Length, 10);
+
+			actual.Should().Be(10);
+		}
+
+		[Fact]
+		public void MapOrElse_OkResult_ShouldReturnMappedValue()
+		{
+			Result<int, string> result = Ok(10);
+
+			var actual = result.MapOrElse(x => x * 3, _ => -1);
+
+			actual.Should().Be(30);
+		}
+
+		[Fact]
+		public void MapOrElse_FaultedResult_ShouldReturnValueFromElseClosure()
+		{
+			Result<string, int> result = Err(1);
+
+			var actual = result.MapOrElse(_ => -1, err => err);
+
+			actual.Should().Be(1);
+		}
+
+		[Fact]
 		public void MapErr_FaultedResult_ShouldReturnFaultedResultWithMappedError()
 		{
 			Result<string, int> result = Err(0);
@@ -120,6 +171,149 @@ namespace ResultSharp.Tests
 			var actual = result.MapErr(err => $"code: {err}");
 
 			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void MapErr_OkResult_ShouldNotMutateResult()
+		{
+			Result<int, string> result = Ok(10);
+
+			var expected = Ok(10);
+			var actual = result.MapErr(_ => -1);
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void And_OkResult_ShouldReturnOtherResult()
+		{
+			Result<int, string> a = Ok(10);
+			Result<int, string> b = Ok(20);
+
+			var expected = Ok(20);
+			var actual = a.And(b);
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void And_FaultedResult_ShouldNotReturnTheOtherResult()
+		{
+			Result<string, int> a = Err(-1);
+			Result<string, int> b = Ok("ok");
+
+			var expected = Err(-1);
+			var actual = a.And(b);
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void AndThen_OkResult_ShouldReturnAMappedResult()
+		{
+			Result<int, string> result = Ok(10);
+
+			var expected = Ok(30);
+			var actual = result.AndThen<int>(x => Ok(x * 3));
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void AndThen_FaultedResult_ShouldNotRunTheAndThenClosure()
+		{
+			Result<int, string> result = Err("err");
+
+			var expected = Err("err");
+			var actual = result.AndThen<int>(x => Ok(x * 3));
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void Or_OkResult_ShouldNotReturnTheOrResult()
+		{
+			Result<int, string> a = Ok(10);
+			Result<int, string> b = Ok(20);
+
+			var expected = Ok(10);
+			var actual = a.Or(b);
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void Or_FaultedResult_ShouldReturnTheOrResult()
+		{
+			Result<int, string> a = Err("err");
+			Result<int, string> b = Ok(20);
+
+			var expected = Ok(20);
+			var actual = a.Or(b);
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void OrElse_OkResult_ShouldNotRunOrClosure()
+		{
+			Result<int, string> result = Ok(10);
+
+			var expected = Ok(10);
+			var actual = result.OrElse<int>(_ => Ok(0));
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void OrElse_FaultedResult_ShouldReturnOrClosure()
+		{
+			Result<string, int> result = Err(-1);
+
+			var expected = Ok("-1");
+			var actual = result.OrElse<int>(err => Ok(err.ToString()));
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void Expect_OkResult_ShouldNotThrowAndShouldReturnValue()
+		{
+			Result<int, string> result = Ok(10);
+
+			Func<int> expect = () => result.Expect("err");
+
+			expect.Should().NotThrow("because the result is ok").Which.Should().Be(10);
+		}
+
+		[Fact]
+		public void Expect_FaultedResult_ShouldThrow()
+		{
+			Result<string, int> result = Err(-1);
+
+			Func<string> expect = () => result.Expect("err");
+
+			expect.Should().Throw<ExpectException>("because the result is faulted");
+		}
+
+		[Fact]
+		public void ExpectErr_OkResult_ShouldThrow()
+		{
+			Result<int, string> result = Ok(10);
+
+			Func<string> expectErr = () => result.ExpectErr("err");
+
+			expectErr.Should().Throw<ExpectException>("because the result is ok");
+		}
+
+		[Fact]
+		public void ExpectErr_FaultedResult_ShouldNotThrowAndShouldReturnErr()
+		{
+			Result<string, int> result = Err(-1);
+
+			Func<int> expectErr = () => result.ExpectErr("err");
+
+			expectErr.Should().NotThrow().Which.Should().Be(-1);
 		}
 	}
 }
