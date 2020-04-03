46c8af06e50b0435eda1fc5f434c7312216ee15e
Vegard Strand Lende <lende.vegard@gmail.com>
2020-04-03 16:24:46 +0200

    Add more tests

--- ResultSharp.Tests/ResultTests.cs
+++ ResultSharp.Tests/ResultTests.cs
@@ -16,12 +16,29 @@ namespace ResultSharp.Tests
 		}
 
 		[Fact]
+		public void Ok_IsErr_ReturnsFalse()
+		{
+			var result = Result<int, string>.Ok(0);
+
+			result.IsErr.Should().BeFalse();
+		}
+
+		[Fact]
 		public void Err_IsOk_ReturnsFalse()
 		{
 			var result = Result<string, int>.Err(0);
 
 			result.IsOk.Should().BeFalse();
 		}
+
+		[Fact]
+		public void Err_IsErr_ReturnsTrue()
+		{
+			var result = Result<string, int>.Err(0);
+
+			result.IsErr.Should().BeTrue();
+		}
+
 
 		[Fact]
 		public void Unwrap_OkResult_ShouldNotThrow()
@@ -41,6 +58,26 @@ namespace ResultSharp.Tests
 			Func<string> unwrapErr = result.UnwrapErr;
 
 			unwrapErr.Should().Throw<UnwrapException>("because the result is ok");
+		}
+
+		[Fact]
+		public void Unwrap_FaultedResult_ShouldThrow()
+		{
+			Result<string, int> result = Err(0);
+
+			Func<string> unwrap = result.Unwrap;
+
+			unwrap.Should().Throw<UnwrapException>("because the result is faulted");
+		}
+
+		[Fact]
+		public void UnwrapErr_FaultedResult_ShouldNotThrow()
+		{
+			Result<string, int> result = Err(0);
+
+			Func<int> unwrapErr = result.UnwrapErr;
+
+			unwrapErr.Should().NotThrow("because the result is faulted");
 		}
 	}
 }
