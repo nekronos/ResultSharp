438b3c6a1a17eaf9cfd0baa1e26dad4df44490bd
Vegard Strand Lende <lende.vegard@gmail.com>
2020-04-06 20:26:01 +0200

    Add PreludeTests

---
+++ ResultSharp.Tests/PreludeTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using static ResultSharp.Prelude;
+
+namespace ResultSharp.Tests
+{
+	public class PreludeTests
+	{
+		class DivideByZeroException : Exception
+		{
+			public override bool Equals(object obj) =>
+				obj is DivideByZeroException ? true : false;
+		}
+
+		static int Divide(int dividend, int divisor) =>
+			divisor switch
+			{
+				0 => throw new DivideByZeroException(),
+				_ => dividend / divisor,
+			};
+
+
+		[Fact]
+		public void Try_CatchesException_ReturnsFaultedResult()
+		{
+			Result<int, Exception> expected = Err<Exception>(new DivideByZeroException());
+
+			Result<int, Exception> actual = Try(() => Divide(10, 0));
+
+			actual.Should().Be(expected);
+		}
+
+		[Fact]
+		public void Try_CatchesSpecificException_ReturnsFaultedResult()
+		{
+			var expected = Err(new DivideByZeroException());
+
+			var actual = Try<int, DivideByZeroException>(() => Divide(10, 0));
+
+			actual.Should().Be(expected);
+		}
+	}
+}
