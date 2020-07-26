189e85e1ef690d2698fd4b6f3a4ca382bac188c2
Vegard Strand Lende <lende.vegard@gmail.com>
2020-07-26 18:33:21 +0200

    Add ResultExtensionsTests

---
+++ ResultSharp.Tests/ResultExtensionsTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FluentAssertions;
+using Xunit;
+using static ResultSharp.Prelude;
+
+namespace ResultSharp.Tests
+{
+	public class ResultExtensionsTests
+	{
+		[Fact]
+		public void Combine_ArrayOfResults_ReturnsResultOfIEnumerableContainingErrors()
+		{
+			IEnumerable<Result<string, int>> results = new Result<string, int>[]
+			{
+				Ok("foo"),
+				Err(1),
+				Ok("bar"),
+				Err(2),
+				Ok("baz"),
+				Err(3)
+			};
+
+			var expected = new[] { 1, 2, 3 };
+			var actual = results.Combine().UnwrapErr();
+
+			actual.Should().BeEquivalentTo(expected);
+		}
+
+		[Fact]
+		public void Combine_ArrayOfResults_ReturnsCombinedResultWithOkValues()
+		{
+			IEnumerable<Result<string, int>> results = new Result<string, int>[]
+			{
+				Ok("foo"),
+				Ok("bar"),
+				Ok("baz"),
+			};
+
+			var expected = new[] { "foo", "bar", "baz" };
+			var actual = results.Combine().Unwrap();
+
+			actual.Should().BeEquivalentTo(expected);
+		}
+
+		[Fact]
+		public void AndThenTry_TryChain_ReturnsFaultedResult()
+		{
+			var expected = Err(new DivideByZeroException());
+
+			var actual = Result<int, DivideByZeroException>
+				.Ok(10)
+				.AndThenTry(x => TestStubs.Divide(x, 0));
+
+			actual.Should().Be(expected);
+		}
+	}
+}
